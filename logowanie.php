0a9a8133886176e67f96772cb9e16e5086a5b39f
MariiaS3 <mariia.sydor28082001@gmail.com>
2023-04-06 23:54:58 +0200

    token

--- logowanie.php
+++ logowanie.php
@@ -46,8 +46,24 @@
                                 $rekord = $find_user->fetch_array();
                                 if ($rekord['password'] == $password_hash) {
                                     echo "zalogowano";
+                                    $secretKey  = 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=';
+                                    $issuedAt   = new DateTimeImmutable();
+                                    $expire     = $issuedAt->modify('+6 minutes')->getTimestamp();
+                                    $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+                                    $payload = json_encode([
+                                        'iat'  => $issuedAt->getTimestamp(),                          
+                                        'nbf'  => $issuedAt->getTimestamp(),         
+                                        'exp'  => $expire, 
+                                        'user_name' => $email ]);    
+                                    $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+                                    $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+                                    $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secretKey, true);
+                                    $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+                                    $token = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+                                    
                                     session_start();
-                                    // $_SESSION['użutkownik']=token;
+                                    $_SESSION['użutkownik']='Bearer '+$token;
+
                                 } else {
                                     header("Location:rejestracja.html", TRUE, 401);
                                 }

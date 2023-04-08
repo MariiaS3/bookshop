57b5e8a42b8da7766bd2928a064ac5b0e7809616
MariiaS3 <mariia.sydor28082001@gmail.com>
2023-04-08 05:20:35 +0200

    kosz

--- koszyk.php
+++ koszyk.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./main.css">
+    <link rel="stylesheet" href="./koszyk.css">
 </head>
 
 <body>
@@ -19,67 +20,55 @@
         $user = "root";
         $password = "";
         $dbname = "bookshop";
-        if(!isset($_SESSION['koszyk'])){
+
+        session_start();
+        if (!isset($_SESSION['koszyk'])) {
             echo "<p>Koszyk jest pusty</p>";
-        }else{
+        } else {
             try {
 
-                $koszyk=$_SESSION['koszyk'];
-                $isbn=[];
-                $n=[];
-                for ($i=0, $j=1; $i<count($koszyk); $i+=2, $j+=2) {
+                $koszyk = $_SESSION['koszyk'];
+                $isbn = [];
+                $n = [];
+               
+                for ($i = 0, $j = 1; $i < count($koszyk); $i += 2, $j += 2) {
                     // echo '<p>'.$koszyk[$i]." => ".$koszyk[$j].'</p>';
                     array_push($isbn, $koszyk[$i]);
                     array_push($n, $koszyk[$j]);
                 }
+                $count = count($isbn);
+                $placeholders = implode(',', array_fill(0, $count, '?'));
+
                 echo "<a href=\"kasuj.php\">Kasuj koszyk</a>";
-                
+
                 $id = new mysqli($address, $user, $password, $dbname);
 
-                $query = "SELECT * FROM books WHERE isbn IN '.$isbn.';";
+                $query = "SELECT * FROM books WHERE isbn IN ($placeholders)";
                 $dane = $id->query($query);
         ?>
                 <div class="container">
                     <div class="items">
                         <ul>
                             <?php
-                            $m = 1;
-                            while ($rekord = $dane->fetch_array()) {
+                            $j = 0;
+                            while ($rekord = $dane->fetch_assoc()) {
                             ?>
                                 <li class="item">
                                     <div class="opis">
-                                        <div>
-                                            <?php
-                                            echo '<img src="./img/book' . $m . '.png">';
-
-                                            echo '<p></p>';
-                                            ?>
-                                        </div>
                                         <div class="tytul">
                                             <?php
                                             echo '<h2 class="tytul">' . $rekord['tytul'] . '</h2>';
                                             echo '<p class="dane">Autor: ' . $rekord['autor'] . '</p>';
                                             echo '<p class="dane">ISBN: ' . $rekord['isbn'] . '</p>';
+                                            echo '<p class="dane">Cena : ' . $rekord['cena'] . '</p>';
+                                            echo '<p class="dane">Ilość : ' . $n[$j] . '</p>';
+                                            echo '<p class="dane">Wartość : ' . $rekord['cena'] * $n[$j] . '</p>';
                                             ?>
-                                        </div>
-                                        <div class="cena">
-                                            <?php
-                                            echo '<h3>' . $rekord['cena'] . ' zł</h3>';
-                                            ?>
-                                            <form method='get' action="#">
-                                                <input type="hidden" name="id_book" value="<?= $rekord['isbn'] ?>">
-                                                <input class="dodaj-do-kosz" type="submit" value="Dodaj do koszyka" /><br>
-                                                <input type="number" class="num" name="num" value="1" min="0" max="999">
-                                                <i class="stan"> z <?= $rekord['stan'] ?> sztuk</i>
-                                            </form>
                                         </div>
                                     </div>
                                 </li>
                             <?php
-                                $m++;
-                                if ($m >= 8) {
-                                    $m = 1;
-                                }
+                                $j++;
                             }
                             ?>
                         </ul>
